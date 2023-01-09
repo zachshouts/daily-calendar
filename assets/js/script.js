03c7548b0945d1311135251c066ce378fe5211bf
zachshouts <117788174+zachshouts@users.noreply.github.com>
2023-01-09 14:34:03 -0600

    Implemented dayjs to receive current date/time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
@@ -21,8 +12,13 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    const currentHour = dayjs().hour();
+    const currentDate = dayjs().format('dddd, MMMM D YYYY');
+    const timeContainer = $('#currentDay');
+
     const events = JSON.parse(localStorage.getItem('events') || '[]');
     const saveBtn = $('.saveBtn');
+
     saveBtn.click(function (e) {
         const saveHour = $(this).parent().attr('id');
         console.log(saveHour);
